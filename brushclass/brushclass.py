8a35108ab87d4c3cc80305f1f17336dfe1c90598
gunjianpan <iofu728@163.com>
2019-03-28 00:35:29 +0800

    🐥 brush class pku

--- brushclass/brushclass.py
+++ brushclass/brushclass.py
@@ -2,23 +2,23 @@
 # @Author: gunjianpan
 # @Date:   2019-02-25 21:13:45
 # @Last Modified by:   gunjianpan
-# @Last Modified time: 2019-02-26 20:34:01
+# @Last Modified time: 2019-03-27 23:44:02
 
-import codecs
-import threading
 import time
-import pandas as pd
-import re
-import requests
 import random
-import smtplib
 
-
-from email.mime.text import MIMEText
-from bs4 import BeautifulSoup
 from proxy.getproxy import GetFreeProxy
-from utils.utils import begin_time, get_html, end_time, changeCookie, changeHeaders, changeHtmlTimeout
-from urllib.request import urlopen
+from utils.utils import begin_time, end_time, send_email
+
+get_request_proxy = GetFreeProxy().get_request_proxy
+data_path = 'brushclass/data/'
+
+"""
+  * brush @http
+  * www.zhihu.com/api/v4/creator/content_statistics
+  * www.jianshu.com/u/
+  * blog.csdn.net
+"""
 
 
 class Brush(object):
@@ -27,49 +27,8 @@ class Brush(object):
     """
 
     def __init__(self, ):
-        self.proxyclass = GetFreeProxy()
         self.failured_map = {}
         self.laster_timestamp = 0
-
-    def send_email(self, context, subject):
-
-        with open('brushclass/data/email', 'r') as f:
-            email = f.readlines()
-        mail_host = 'smtp.163.com'
-        # 163用户名
-        mail_user = email[0][:-1]
-        # 密码(部分邮箱为授权码)
-        mail_pass = email[1][:-1]
-        # 邮件发送方邮箱地址
-        sender = email[0][:-1] + '@163.com'
-        # 邮件接受方邮箱地址，注意需要[]包裹，这意味着你可以写多个邮件地址群发
-        receivers = [email[2][:-1] + '@163.com']
-
-        # 设置email信息
-        # 邮件内容设置
-        message = MIMEText(context, 'plain', 'utf-8')
-        # 邮件主题
-        message['Subject'] = subject
-        # 发送方信息
-        message['From'] = sender
-        # 接受方信息
-        message['To'] = receivers[0]
-
-        # 登录并发送邮件
-        try:
-            smtpObj = smtplib.SMTP_SSL()
-            # 连接到服务器
-            smtpObj.connect(mail_host, 465)
-            # 登录到服务器
-            smtpObj.login(mail_user, mail_pass)
-            # 发送
-            smtpObj.sendmail(
-                sender, receivers, message.as_string())
-            # 退出
-            smtpObj.quit()
-            print('success')
-        except smtplib.SMTPException as e:
-            print('error', e)  # 打印错误
 
     def have_places(self):
         """
@@ -80,9 +39,9 @@ class Brush(object):
 
         while not have_places:
             if self.have_places_once():
-                self.send_email('大数据专题', '大数据专题 有名额啦 有名额啦')
-                self.send_email('大数据专题', '大数据专题 有名额啦 有名额啦')
-                self.send_email('大数据专题', '大数据专题 有名额啦 有名额啦')
+                send_email('大数据专题', '大数据专题 有名额啦 有名额啦')
+                send_email('大数据专题', '大数据专题 有名额啦 有名额啦')
+                send_email('大数据专题', '大数据专题 有名额啦 有名额啦')
                 have_places = True
             time.sleep(random.randint(10, 20))
         end_time = ()
@@ -92,15 +51,10 @@ class Brush(object):
         have places
         """
         url = 'http://elective.pku.edu.cn/elective2008/edu/pku/stu/elective/controller/supplement/refreshLimit.do'
-        with open('brushclass/data/cookie', 'r') as f:
+        with open(data_path + 'cookie', 'r') as f:
             cookie = f.readlines()
         headers = {
             'pragma': 'no-cache',
-            # 'sec-fetch-dest': 'empty',
-            # 'sec-fetch-site': 'same-origin',
-            # 'sec-fetch-user': '?F',
-            # 'sec-origin-policy': '0',
-            # 'upgrade-insecure-requests': '1',
             'X-Requested-With': 'XMLHttpRequest',
             'cache-control': 'no-cache',
             'Cookie': '',
@@ -114,19 +68,18 @@ class Brush(object):
             "Origin": "http://elective.pku.edu.cn",
             "Referer": "http://elective.pku.edu.cn/elective2008/edu/pku/stu/elective/controller/supplement/SupplyCancel.do",
         }
-        changeHeaders(headers)
-        changeCookie(cookie[0][:-1])
+        headers['Cookie'] = cookie[0][:-1]
 
         data = {
             "index": '10',
             "seq": 'yjkc20141100016542',
         }
 
-        ca = self.proxyclass.get_request_proxy(url, 3, data)
+        ca = get_request_proxy(url, 11, data, header=headers)
 
         if not ca:
             if round(time.time()) - self.laster_timestamp > 60:
-                self.send_email("Cookie failure", "Cookie failure")
+                send_email("Cookie failure", "Cookie failure")
             return False
         print(ca['electedNum'])
         self.laster_timestamp = round(time.time())

dbb702f7e1393d69337ab8c3a131b5fa678a6aba
gunjianpan <iofu728@163.com>
2019-09-10 21:14:17 +0800

    👹  finial to db

--- buildmd/activateArticle.py
+++ buildmd/activateArticle.py
@@ -2,7 +2,7 @@
 # @Author: gunjianpan
 # @Date:   2019-08-26 20:46:29
 # @Last Modified by:   gunjianpan
-# @Last Modified time: 2019-09-09 01:50:43
+# @Last Modified time: 2019-09-10 21:13:44
 
 import hashlib
 import json
@@ -14,6 +14,7 @@ import urllib
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from configparser import ConfigParser
 
+import numpy as np
 import regex
 
 sys.path.append(os.getcwd())
@@ -43,13 +44,14 @@ class ActivateArticle(object):
     MTOP_URL = 'https://h5api.m.taobao.com/h5/mtop.alimama.union.xt.en.api.entry/1.0/'
     S_ARTICLE_SQL = 'SELECT `id`, article_id, tpwd_id, item_id, tpwd, domain, content, url, expire_at, created_at from article_tpwd WHERE `article_id` = "%s";'
     I_ARTICLE_SQL = 'INSERT INTO article_tpwd (article_id, tpwd_id, item_id, tpwd, domain, content, url, expire_at) VALUES %s;'
-    R_ARTICLE_SQL = 'REPLACE INTO article_tpwd (article_id, tpwd_id, item_id, tpwd, domain, content, url, expire_at, created_at) VALUES %s;'
+    R_ARTICLE_SQL = 'REPLACE INTO article_tpwd (`id`, article_id, tpwd_id, item_id, tpwd, domain, content, url, expire_at, created_at, is_deleted) VALUES %s;'
     JSON_KEYS = ['p', 'ct', 'su', 'pr', 'au', 'pv',
                  'mt', 'sz', 'domain', 'tl', 'content']
     URL_DOMAIN = {0: 's.click.taobao.com',
                   1: 'item.taobao.com',
                   5: 'uland.taobao.com',
-                  10: 'taoquan.taobao.com'}
+                  10: 'taoquan.taobao.com',
+                  15: 'empty'}
     NEED_KEY = ['content', 'url', 'validDate']
     ONE_HOURS = 3600
     ONE_DAY = 24
@@ -62,6 +64,7 @@ class ActivateArticle(object):
         self.Db = Db('tbk')
         self.Db.create_table(os.path.join(root_dir, 'tpwd.sql'))
         self.tpwd_map = {}
+        self.tpwd_db_map = {}
         self.tpwds = {}
         self.cookies = {}
         self.tpwd_exec = ThreadPoolExecutor(max_workers=50)
@@ -112,6 +115,12 @@ class ActivateArticle(object):
             echo(3, 'return result')
             return req['content']
 
+    def load_article_pipeline(self):
+        article_exec = ThreadPoolExecutor(max_workers=5)
+        a_list = [article_exec.submit(self.load_article, ii)
+                  for ii in self.idx]
+        list(as_completed(a_list))
+
     def load_article(self, article_id: int):
         article = self.basic_youdao(article_id)
         tpwds = list(set(regex.findall('\p{Sc}(\w{8,12}?)\p{Sc}', article)))
@@ -119,6 +128,7 @@ class ActivateArticle(object):
         if not article_id in self.tpwd_map:
             self.tpwd_map[article_id] = {}
         time = 0
+        au_list = []
         while len(self.tpwd_map[article_id]) < len(tpwds) and time < 5:
             thread_list = [ii for ii in tpwds
                            if not ii in self.tpwd_map[article_id]]
@@ -127,23 +137,50 @@ class ActivateArticle(object):
             thread_list = [self.tpwd_exec.submit(self.decoder_tpwd_once,
                                                  article_id, ii) for ii in thread_list]
             list(as_completed(thread_list))
-            au_list = [self.tpwd_exec.submit(self.decoder_tpwd_url,
-                                             article_id, ii) for ii, jj in self.tpwd_map[article_id].items() if not 'type' in jj or jj['item_id'] is None]
-            list(as_completed(au_list))
+            au_list.extend([self.tpwd_exec.submit(self.decoder_tpwd_url,
+                                                  article_id, ii) for ii, jj in self.tpwd_map[article_id].items()
+                            if not 'type' in jj or jj['item_id'] is None])
             time += 1
+        list(as_completed(au_list))
         self.load_article2db(article_id)
 
     def load_article2db(self, article_id: str):
         m = self.tpwd_map[article_id]
-        ##exist_list = self.db.select_db(self.S_ARTICLE_SQL % article_id)
-        data = [(article_id, ii, m[jj]['item_id'], jj, m[jj]['type'], m[jj]['content'], m[jj]['url'], str(int(m[jj]['validDate']))) for ii, jj in enumerate(self.tpwds[article_id])]
-        i_sql = self.I_ARTICLE_SQL % str(data)[1:-1]
+        exist_list = [ii for ii in self.Db.select_db(
+            self.S_ARTICLE_SQL % article_id)]
+        for ii, jj in enumerate(exist_list):
+            t = jj[-1].strftime('%Y-%m-%d %H:%M:%S')
+            exist_list[ii] = (*jj[:-1], t)
+        self.tpwd_db_map[article_id] = {ii[2]: ii for ii in exist_list}
+        data = [(article_id, ii, m[jj]['item_id'], jj, m[jj]['type'], m[jj]['content'], m[jj]['url'], str(int(m[jj]['validDate'])))
+                for ii, jj in enumerate(self.tpwds[article_id]) if jj in m and 'item_id' in m[jj] and m[jj]['type'] != 15]
+        data_map = {ii[1]: ii for ii in data}
+        update_list, insert_list = [], []
+        for ii in data:
+            if ii[1] in self.tpwd_db_map[article_id]:
+                t = self.tpwd_db_map[article_id][ii[1]]
+                update_list.append((t[0], *ii, t[-1], 0))
+            else:
+                insert_list.append(ii)
+        for ii, jj in self.tpwd_db_map[article_id].items():
+            if ii not in data_map:
+                update_list.append((*jj, 1))
+        self.update_db(article_id, insert_list, 'Insert')
+        self.update_db(article_id, update_list, 'Update')
+
+    def update_db(self, article_id: str, data: list, types: str):
+        if not len(data):
+            return
+        if 'insert' in types.lower():
+            i_sql = self.I_ARTICLE_SQL % str(data)[1:-1]
+        else:
+            i_sql = self.R_ARTICLE_SQL % str(data)[1:-1]
         insert_re = self.Db.insert_db(i_sql)
         if insert_re:
-            echo(1, 'Insert article_id {} {} info  Success'.format(
-                article_id, len(data)))
+            echo(1, '{} article_id {} {} info Success'.format(
+                types, article_id, len(data)))
         else:
-            echo(0, 'Insert article_id {} failed'.format(article_id))
+            echo(0, '{} article_id {} failed'.format(types, article_id))
 
     def decoder_tpwd_once(self, article_id: str, tpwd: str):
         req = self.decoder_tpwd(tpwd)
@@ -163,8 +200,6 @@ class ActivateArticle(object):
         temp_map = self.tpwd_map[article_id][tpwd]
         tpwd_type, item_id = self.analysis_tpwd_url(temp_map['url'])
         if item_id is None:
-            if can_retry(temp_map['url']):
-                self.decoder_tpwd_url(article_id, tpwd)
             return
         temp_map['type'] = tpwd_type
         temp_map['item_id'] = item_id
@@ -182,6 +217,8 @@ class ActivateArticle(object):
             return 10, 0
         elif self.URL_DOMAIN[1] in url:
             return 1, self.get_item_detail(url)
+        elif url == '':
+            return 15, 0
         echo('0|warning', 'New Domain:',
              regex.findall('https://(.*?)/', url), url)
         return 20, 0
@@ -203,18 +240,36 @@ class ActivateArticle(object):
 
     def get_s_click_url(self, s_click_url: str):
         ''' decoder s.click real jump url @validation time: 2019.08.31'''
-        req = proxy_req(s_click_url, 2)
-        if req is None:
-            if can_retry(s_click_url):
-                return self.get_s_click_url(s_click_url)
-            else:
-                return
-        tu_url = req.url
+        if 'tu=' not in s_click_url:
+            tu_url = self.get_s_click_tu(s_click_url)
+        else:
+            tu_url = s_click_url
+        if tu_url is None or 'tu=' not in tu_url:
+            echo(3, 's_click_url tu url ENd Retry..')
+            return
         qso = decoder_url(tu_url)
         if 'tu' not in qso:
-            echo(0, s_click_url, tu_url)
+            if 'alisec' in tu_url:
+                echo('0|debug', 'Request Too Fast')
+                time.sleep(np.random.randint(10) * np.random.rand())
+            else:
+                echo(0, s_click_url, tu_url)
+            return
         redirect_url = urllib.parse.unquote(qso['tu'])
         return self.get_s_click_detail(redirect_url, tu_url)
+
+    def get_s_click_tu(self, s_click_url: str):
+        headers = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+            'Host': 's.click.taobao.com'
+        }
+        req = proxy_req(s_click_url, 2, header=headers)
+        if req is None or 'tu=' not in req.url:
+            if can_retry(s_click_url):
+                return self.get_s_click_tu(s_click_url)
+            else:
+                return
+        return req.url
 
     def get_s_click_detail(self, redirect_url: str, tu_url: str):
         headers = {
@@ -222,7 +277,7 @@ class ActivateArticle(object):
             'referer': tu_url
         }
         req = proxy_req(redirect_url, 2, header=headers)
-        if req is None:
+        if req is None or 'id=' not in req.url:
             if can_retry(redirect_url):
                 return self.get_s_click_detail(redirect_url, tu_url)
             else:

310989e6febda5a8bc097bad6f493248fb45c7dd
gunjianpan <iofu728@163.com>
2019-09-26 02:00:01 +0800

    🏯change qrticle_tpwd more robost

--- buildmd/tpwd.sql
+++ buildmd/tpwd.sql
@@ -7,8 +7,13 @@ CREATE TABLE if not exists `article_tpwd` (
   `domain` int(10) unsigned NOT NULL DEFAULT 0 COMMENT 'tpwd type @0->s.click, @1->item, @5->uland, @10->taoquan',
   `content` varchar(300) NOT NULL DEFAULT '_' COMMENT 'tpwd content', 
   `url` varchar(1000) NOT NULL DEFAULT '_' COMMENT 'tpwd corresponding url',
+  `commission_rate` int(10) unsigned NOT NULL DEFAULT 0 COMMENT 'commission rate',
+  `commission_type` varchar(30) NOT NULL DEFAULT '' COMMENT 'commission type',
   `expire_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'expire time',
   `is_deleted` int(10) unsigned NOT NULL DEFAULT 0 COMMENT 'is deleted',
+  `other1` varchar(300) NOT NULL DEFAULT '' COMMENT 'other1',
+  `other2` varchar(300) NOT NULL DEFAULT '' COMMENT 'other2',
+  `other3` varchar(300) NOT NULL DEFAULT '' COMMENT 'other3',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'create time',
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'update time',
   PRIMARY KEY (`id`)
